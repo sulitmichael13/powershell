1bf81705a07ee340e82a3836675a4f37878175d7
Michael Sulit <michael.sulit@igentechnologies.com.ph>
2020-02-26 10:45:50 +0800

    Adding my first script to github

---
+++ MyScriptPS.ps1
@@ -0,0 +1,18 @@
+﻿# Get available commands from powershell
+
+Get-Process -ComputerName localhost
+
+#to get process specifically for Examples
+help Get-Process -Examples
+
+#to update help system
+Update-Help -Force -Verbose
+
+#
+Get-Process -Name *notepad* | Stop-Process -Force
+
+Get-Command -Verb Get -Noun *serv*
+
+Get-Module -ListAvailable
+
+Get-Process -Name dasHost 
